bd8d4edf2ea1097ba14edbd63072be2fd882006b
Julius <juliushaas91@gmail.com>
2024-12-12 00:07:04 +0530

    feat: add check if entities in setup-gcloud exist already

--- Taskfile.sh
+++ Taskfile.sh
@@ -3,6 +3,7 @@ set -e
 
 # Load environment variables for Google Cloud configuration
 source config.env
+
 # //////////////////////////////////////////////////////////////////////////////
 # START tasks
 
@@ -12,7 +13,7 @@ run-application() {
 
 setup-project() {
   echo "...installing uv..."
-  pip install uv
+  brew install uv
   echo "...setting up project environment..."
   uv sync --frozen --no-cache
 }
@@ -30,13 +31,11 @@ validate() {
 }
 
 deploy() {
-
   echo "... fetching project variables ..."
   NAME="$(uv run python3 -c "import toml; print(toml.load('pyproject.toml')['project']['name'])")"
   VERSION="$(uv run python3 -c "import toml; print(toml.load('pyproject.toml')['project']['version'])")"
   DATETIME=$(uv run date +"%y-%m-%d-%H%M%S")
   IMAGE_TAG="${SERVICE_REGION}-docker.pkg.dev/${GCP_PROJECT_ID}/docker/${NAME}:${VERSION}-${DATETIME}"
-  # NOTE: all other env variables are imported via "source config.env" (line 5)
 
   echo "... building docker image ..."
   uv run gcloud auth configure-docker "${SERVICE_REGION}-docker.pkg.dev"
@@ -76,47 +75,72 @@ create-identity-token() {
 }
 
 setup-gcloud() {
-  # SOURCE: https://github.com/google-github-actions/auth#indirect-wif
-  # info: all other env variables are imported via "source config.env" (line 5)
   echo "--- SETTING UP GOOGLE CLOUD INFRASTRUCTURE ---"
-  echo "creating service account: ${CLOUD_RUN_SERVICE_ACCOUNT}"
-  uv run gcloud iam service-accounts create "${CLOUD_RUN_SERVICE_ACCOUNT}" \
-  --project "${GCP_PROJECT_ID}"
+  
+  # Check if service account exists
+  if ! gcloud iam service-accounts list --project="${GCP_PROJECT_ID}" --filter="email:${SA_CLOUD_RUN}@${GCP_PROJECT_ID}.iam.gserviceaccount.com" --format="value(email)" | grep -q "${SA_CLOUD_RUN}"; then
+    echo "Creating service account: ${CLOUD_RUN_SERVICE_ACCOUNT}"
+    gcloud iam service-accounts create "${SA_CLOUD_RUN}" --project "${GCP_PROJECT_ID}"
+    echo "SA_CLOUD_RUN=\"${SA_CLOUD_RUN}\"" >> config.env
+  else
+    echo "Service account ${CLOUD_RUN_SERVICE_ACCOUNT} already exists"
+  fi
 
-  echo "writing 'SERVICE_ACCOUNT=${CLOUD_RUN_SERVICE_ACCOUNT}' to config.env"
-  echo "SERVICE_ACCOUNT=\"${CLOUD_RUN_SERVICE_ACCOUNT}\"" >> config.env
+  # Check if workload identity pool exists
+  if ! gcloud iam workload-identity-pools list --project="${GCP_PROJECT_ID}" --location="global" --format="value(name)" | grep -q "github"; then
+    echo "Creating workload-identity-pool in project ${GCP_PROJECT_ID}"
+    gcloud iam workload-identity-pools create "github" \
+    --project="${GCP_PROJECT_ID}" \
+    --location="global" \
+    --display-name="GitHub Actions Pool"
+  else
+    echo "Workload identity pool 'github' already exists"
+  fi
 
-  echo "creating workload-identity-pool in project ${GCP_PROJECT_ID}"
-  uv run gcloud iam workload-identity-pools create "github" \
-  --project="${GCP_PROJECT_ID}" \
-  --location="global" \
-  --display-name="GitHub Actions Pool"
-
-  echo "checking for created workload-identity-pools in project ${GCP_PROJECT_ID}"
-  WIF_POOL_NAME=$(uv run gcloud iam workload-identity-pools describe "github" \
+  echo "Retrieving workload-identity-pool name in project ${GCP_PROJECT_ID}"
+  WIF_POOL_NAME=$(gcloud iam workload-identity-pools describe "github" \
   --project="${GCP_PROJECT_ID}" \
   --location="global" \
   --format="value(name)")
-  echo "success: workload-identity-pool ${WIF_POOL_NAME} exists"
+  echo "Workload-identity-pool ${WIF_POOL_NAME} already exists"
 
   REPO_NAME=$(cat pyproject.toml | grep "^name\s*=" | cut -d'=' -f2 | tr -d ' "' | head -n1)
 
-  echo "adding iam-policy-binding for ${CLOUD_RUN_SERVICE_ACCOUNT} in project ${GCP_PROJECT_ID}"
-  uv run "gcloud iam service-accounts add-iam-policy-binding ${CLOUD_RUN_SERVICE_ACCOUNT}" \
+  # Check if IAM policy binding exists
+  if ! gcloud iam service-accounts get-iam-policy "${CLOUD_RUN_SERVICE_ACCOUNT}" \
     --project="${GCP_PROJECT_ID}" \
-    --role="roles/iam.workloadIdentityUser" \
-    --member="principalSet://iam.googleapis.com/${WIF_POOL_NAME}/attribute.repository/${GITHUB_ORG}/${REPO_NAME}"
+    --format="json" | jq -e ".bindings[] | select(.role == \"roles/iam.workloadIdentityUser\" and (.members | type == \"array\") and (.members[] | tostring | contains(\"principalSet://iam.googleapis.com/${WIF_POOL_NAME}/attribute.repository/${GITHUB_ORG}/${REPO_NAME}\")))" > /dev/null; then
+    echo "Adding iam-policy-binding for ${CLOUD_RUN_SERVICE_ACCOUNT} in project ${GCP_PROJECT_ID}"
+    gcloud iam service-accounts add-iam-policy-binding "${CLOUD_RUN_SERVICE_ACCOUNT}" \
+      --project="${GCP_PROJECT_ID}" \
+      --role="roles/iam.workloadIdentityUser" \
+      --member="principalSet://iam.googleapis.com/${WIF_POOL_NAME}/attribute.repository/${GITHUB_ORG}/${REPO_NAME}"
+  else
+      echo "IAM policy binding already exists for ${CLOUD_RUN_SERVICE_ACCOUNT}"
+  fi
 
-  WIF_PROVIDER_NAME=$(uv run gcloud iam workload-identity-pools providers describe "${REPO_NAME}" \
+  # Check if workload identity provider exists
+  if ! gcloud iam workload-identity-pools providers list --project="${GCP_PROJECT_ID}" --location="global" --workload-identity-pool="github" --format="value(name)" | grep -q "${REPO_NAME}"; then
+    echo "Creating workload identity provider ${REPO_NAME}"
+    gcloud iam workload-identity-pools providers create-oidc "${REPO_NAME}" \
+    --project="${GCP_PROJECT_ID}" \
+    --location="global" \
+    --workload-identity-pool="github" \
+    --issuer-uri="https://token.actions.githubusercontent.com" \
+    --attribute-mapping="google.subject=assertion.sub,attribute.actor=assertion.actor,attribute.repository=assertion.repository" \
+    --attribute-condition="assertion.repository_owner == '${GITHUB_ORG}'"
+  else
+    echo "Workload identity provider ${REPO_NAME} already exists"
+  fi
+
+  WIF_PROVIDER_NAME=$(gcloud iam workload-identity-pools providers describe "${REPO_NAME}" \
   --project="${GCP_PROJECT_ID}" \
   --location="global" \
   --workload-identity-pool="github" \
   --format="value(name)")
 
-  echo "checking for workload-identity-provider name in project ${GCP_PROJECT_ID}"
-  echo "success: workload-identity-provider ${WIF_PROVIDER_NAME} was retrieved"
-  echo "writing 'WIF_PROVIDER_NAME=${WIF_PROVIDER_NAME}' to config.env"
-  echo "WIF_PROVIDER_NAME=\"${WIF_PROVIDER_NAME}\"" >> config.env
+  echo "Workload-identity-provider ${WIF_PROVIDER_NAME} retrieved"
+  echo "WIF_PROVIDER=\"${WIF_PROVIDER_NAME}\"" >> config.env
   echo "SUCCESS: SETTING UP GOOGLE CLOUD INFRASTRUCTURE IS COMPLETE"
 }
 
